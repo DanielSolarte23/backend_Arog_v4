c4a0edc0015371424edf8d7fb60610a6af018507
DanielSolarte23 <royersolarte22@gmail.com>
2025-03-04 18:07:44 -0500

    Rutas

--- src/routes/ubicacionesRoutes.js
+++ src/routes/ubicacionesRoutes.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-const ubicacionesController = require('../controllers/ubicacionesControllers');
-
-router.post('/', ubicacionesController.crearUbicacion);
-router.get('/', ubicacionesController.obtenerUbicaciones);
-router.get('/:id', ubicacionesController.obtenerUbicacionesporId);
-router.put('/:id', ubicacionesController.actualizarUbicacion);
-router.delete('/:id', ubicacionesController.eliminarUbicaciones);
-=======
 // routes/ubicacionesRoutes.js
 const express = require('express');
 const router = express.Router();
@@ -20,6 +9,5 @@ router.get('/:id', ubicacionesController.getUbicacionById);
 router.post('/', ubicacionesController.createUbicacion);
 router.put('/:id', ubicacionesController.updateUbicacion);
 router.delete('/:id', ubicacionesController.deleteUbicacion);
->>>>>>> 6ccab0bf1120c9cdc18c6e371fa74a7222f62dc5
 
 module.exports = router;
