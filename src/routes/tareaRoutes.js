56304837bf19d24bd1a5d698fb405e4df380ea6d
DanielSolarte23 <royersolarte22@gmail.com>
2025-03-14 18:28:24 -0500

    tareas

--- src/routes/tareaRoutes.js
+++ src/routes/tareaRoutes.js
@@ -7,7 +7,7 @@ router.post('/', tareaController.create);
 router.post('/tareas-con-formulario', tareaController.crearTareaConFormulario);
 router.get('/', tareaController.getAll);
 router.get('/:id', tareaController.getById);
-router.get('/usuario/:usuarioId', tareaController.getByUsuario);
+router.get('/tarea/:usuarioId', tareaController.getByUsuario);
 router.put('/:id', tareaController.update);
 router.patch('/:id', tareaController.updateEstado);
 router.delete('/:id', tareaController.delete);
