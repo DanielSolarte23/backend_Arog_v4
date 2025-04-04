cda75e0b3f3f07308ef46ff5b7a55f820d2d0de9
DanielSolarte23 <royersolarte22@gmail.com>
2025-04-04 13:44:40 -0500

    conexion a neon

--- prisma/migrations/20250403040645_google_auth/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE `usuario` ADD COLUMN `fecha_registro` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    ADD COLUMN `resetPasswordExpires` DATETIME(3) NULL,
-    ADD COLUMN `resetPasswordToken` VARCHAR(100) NULL,
-    ADD COLUMN `verificationToken` VARCHAR(100) NULL,
-    ADD COLUMN `verified` BOOLEAN NOT NULL DEFAULT false;
