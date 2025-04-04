621f0deac6a17e263a430434b20aa21a522d3d35
DanielSolarte23 <royersolarte22@gmail.com>
2025-04-03 21:40:08 -0500

    proteccion rutas usuario

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
-
+const { authRequire } = require('../middlewares/validateToken')
 
 router.post('/', usuarioController.crearUsuario);
-router.get('/', usuarioController.mostrarUsuarios);
+router.get('/', authRequire, usuarioController.mostrarUsuarios);
 router.get('/:id', usuarioController.mostrarUsuario);
 router.put('/:id', usuarioController.actualizarUsuario);
 router.delete('/:id', usuarioController.eliminarUsuario);
