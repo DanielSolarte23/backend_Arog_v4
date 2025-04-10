51642ffb42576abca7e0216ea094346e77e34b64
DanielSolarte23 <royersolarte22@gmail.com>
2025-04-09 23:33:15 -0500

    prisma config

--- prisma/migrations/20250408193512_documentos_pdf/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - The values [CERTIFICADO,INFORME] on the enum `DocumentoPdf_tipo_documento` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `documentopdf` MODIFY `tipo_documento` ENUM('informe', 'certificado') NOT NULL;
